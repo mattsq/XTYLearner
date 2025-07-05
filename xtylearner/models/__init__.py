c6a24c530ce37f0578b28400b5bbb82a8ffb05e0
Matthew Simmons <mattsq@users.noreply.github.com>
2025-07-05 18:51:34 +1000

    Refactor generative trainer and models

--- xtylearner/models/__init__.py
+++ xtylearner/models/__init__.py
@@ -3,11 +3,14 @@
 from .cycle_dual import CycleDual
 from .flow_ssc import MixtureOfFlows
 from .multitask_selftrain import MultiTask
+from .generative import M2VAE, SS_CEVAE
 from .registry import get_model
 
 __all__ = [
     "CycleDual",
     "MixtureOfFlows",
     "MultiTask",
+    "M2VAE",
+    "SS_CEVAE",
     "get_model",
 ]
