8044d23f2affd670a4e24a72ee4a7f7527db8a82
Matthew Simmons <mattsq@users.noreply.github.com>
2025-07-06 16:18:48 +1000

    Add JointEBM model with energy-based training

--- tests/test_trainer.py
+++ tests/test_trainer.py
@@ -5,7 +5,7 @@ from xtylearner.models import CycleDual, MixtureOfFlows, MultiTask
 from xtylearner.training import SupervisedTrainer, GenerativeTrainer, DiffusionTrainer
 from xtylearner.models import M2VAE, SS_CEVAE, JSBF, DiffusionCEVAE
 from xtylearner.models import BridgeDiff, LTFlowDiff
-from xtylearner.models import EnergyDiffusionImputer
+from xtylearner.models import EnergyDiffusionImputer, JointEBM
 
 
 def test_supervised_trainer_runs():
@@ -139,6 +139,19 @@ def test_eg_ddi_trainer_runs():
     model = EnergyDiffusionImputer(d_x=2, d_y=1)
     opt = torch.optim.Adam(model.parameters(), lr=0.001)
     trainer = DiffusionTrainer(model, opt, loader)
+    trainer.fit(1)
+    loss = trainer.evaluate(loader)
+    assert isinstance(loss, float)
+
+
+def test_joint_ebm_trainer_runs():
+    dataset = load_mixed_synthetic_dataset(
+        n_samples=20, d_x=2, seed=12, label_ratio=0.5
+    )
+    loader = DataLoader(dataset, batch_size=5)
+    model = JointEBM(d_x=2, d_y=1)
+    opt = torch.optim.Adam(model.parameters(), lr=0.001)
+    trainer = SupervisedTrainer(model, opt, loader)
     trainer.fit(1)
     loss = trainer.evaluate(loader)
     assert isinstance(loss, float)
